39b6e86a6329ba0ffaf33132a18c34683662e74b
wenjia <dwgia027@gmail.com>
2023-03-28 11:51:00 +1100

    fixed a bug

--- form-input-wave/myFormInputWave/script.js
+++ form-input-wave/myFormInputWave/script.js
@@ -1,14 +1,21 @@
-const labels = document.querySelectorAll("label")
+const labels = document.querySelectorAll(".form-control label")
 
-labels.forEach((label) =>{
+labels.forEach((label) => {
     label.innerHTML = label.innerHTML
-    .split('')
-    .map((letter,index) => {
-        console.log(letter + ' '+ index);
-        return `<span style="transition-delay:${index*0.1}s">${letter}</span>`
-    }).join('')
-    // .map((letter,index) =>{
-    //     `<span style="transition-delay:${index*0.5}s">${letter}</span>`
-    // })
-    // .join('')
+        .split('')
+        .map((letter, index) => {
+            return `<span style="transition-delay:${index * 0.1}s">${letter}</span>`
+        }).join('')
+})
+
+const inputs = document.querySelectorAll("input")
+inputs.forEach((input) => {
+    input.addEventListener("change", () => {
+        if (input.value !== "") {
+            console.log(input.nextElementSibling);
+            input.nextElementSibling.classList.add("stay-up")
+        } else {
+            input.nextElementSibling.classList.remove("stay-up")
+        }
+    })
 })
